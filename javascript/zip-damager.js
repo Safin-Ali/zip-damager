a2e5ad9a98ab4e2dc2649b06da8287e80d0e0841
Safin-Ali <safin.ali.7205@gmail.com>
2023-12-23 23:37:52 +0600

    feat: :sparkles: js code for ui manipulate
    when files select and read complete change ui element styles and behavior

--- javascript/zip-damager.js
+++ javascript/zip-damager.js
@@ -1,1 +1,34 @@
-console.log(`Hello ZDamager`)
+const selectElm = (id) => document.getElementById(id);
+
+const lockBtnElm = selectElm('lockBtn');
+const progValElm = selectElm('progressValue');
+const progBarElm = selectElm('progressBar');
+const inputFile = selectElm('inputFile');
+const fileSizeElm = selectElm('fileSize');
+const feildContElm = selectElm('feildContainer');
+
+// store files as binary
+let filesBin;
+
+inputFile.addEventListener('change', () => {
+	const [files] = inputFile.files;
+	fileSizeElm.innerText = `${Math.floor(files.size / (1024 * 1024))} MB`;
+	lockBtnElm.removeAttribute('disabled');
+
+	const reader = new FileReader();
+
+	reader.onprogress = () => {
+		inputFile.setAttribute('disabled',true)
+	};
+
+	reader.onload = (data) => {
+		lockBtnElm.removeAttribute('disabled');
+		filesBin = data.currentTarget.result;
+	};
+
+	reader.onerror = () => {
+		console.error('Error occurred while reading the file');
+	};
+
+	reader.readAsArrayBuffer(files);
+});
